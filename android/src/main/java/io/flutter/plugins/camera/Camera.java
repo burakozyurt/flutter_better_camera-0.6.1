b79ac7532c652816a4d63fa390dca42ceb447746
Ahmet Burak Özyurt <ozyurtburak11@gmail.com>
2020-11-17 15:45:02 +0300

    Update Camera.java

--- android/src/main/java/io/flutter/plugins/camera/Camera.java
+++ android/src/main/java/io/flutter/plugins/camera/Camera.java
@@ -216,9 +216,9 @@ public class Camera {
 	CamcorderProfile cpHigh = CamcorderProfile.get
 	(CamcorderProfile.QUALITY_HIGH);
 	mediaRecorder.setProfile(cpHigh);
-	mediaRecorder.setVideoEncodingBitRate(2000000);
+	mediaRecorder.setVideoEncodingBitRate(1000000);
 	mediaRecorder.setVideoSize(1280, 720);
-	mediaRecorder.setVideoFrameRate(24);
+	mediaRecorder.setVideoFrameRate(20);
 	mediaRecorder.setOutputFile(outputFilePath);
 	mediaRecorder.setOrientationHint(getMediaOrientation());
     mediaRecorder.prepare();
